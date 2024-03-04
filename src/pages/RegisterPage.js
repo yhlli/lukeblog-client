197555b85e1a0fe64063b63844529647fea2bd1c
Yiheng Li <yiheng.li@hcl.com>
2024-03-04 14:35:27 -0500

    added login and register pages

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,10 @@
 export default function RegisterPage(){
     return(
-        <>
-            <div>Register</div>
-        </>
+        <form className="register">
+            <h1>Register</h1>
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password"/>
+            <button>Register</button>
+        </form>
     );
 }

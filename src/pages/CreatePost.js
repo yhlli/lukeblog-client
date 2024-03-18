546b411e111fa368922a0de646574372904c6b5d
Yiheng Li <yiheng.li@hcl.com>
2024-03-18 16:01:55 -0400

    added update post

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -3,22 +3,7 @@ import ReactQuill from "react-quill"
 import 'react-quill/dist/quill.snow.css';
 import { address } from "../Header";
 import { Navigate } from "react-router-dom";
-
-const modules = {
-    toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
-      ['clean']
-    ]
-};
-const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
-];
+import Editor from "../Editor";
 
 export default function CreatePost(){
     const [title,setTitle] = useState('');
@@ -58,11 +43,7 @@ export default function CreatePost(){
                 onChange={ev => setSummary(ev.target.value)}/>
             <input type="file"
                 onChange={ev => setFiles(ev.target.files)}/>
-            <ReactQuill
-                value={content}
-                onChange={newValue => setContent(newValue)}
-                modules={modules}
-                formats={formats}/>
+            <Editor value={content} onChange={setContent} />
             <button id="createbtn" style={{marginTop:'5px'}}>Create Post</button>
         </form>
     );
--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { address } from "../Header";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { formatISO9075 } from "date-fns";
 import { UserContext } from "../UserContext";
 
@@ -16,7 +16,6 @@ export default function PostPage() {
                 });
             });
     }, []);
-
     if (!postInfo) return '';
     return (
         <div className="post-page">
@@ -25,7 +24,12 @@ export default function PostPage() {
             <div className="author">by {postInfo.author.username}</div>
             {userInfo !== null && userInfo.id === postInfo.author._id && (
                 <div className="edit-row">
-                    <a className="edit-btn" href="">Edit Post</a>
+                    <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                        </svg>
+                        Edit Post
+                    </Link>
                 </div>
             )}
             <div className="image">

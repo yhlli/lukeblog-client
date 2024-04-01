29bfee4f2d3a1762e04d486787e1fe1aea350315
Luke Li <36483872+yhlli@users.noreply.github.com>
2024-04-01 19:12:58 -0400

    loading color

--- src/Loading.js
+++ src/Loading.js
@@ -3,7 +3,7 @@ import CircularProgress from '@mui/joy/CircularProgress';
 export default function Loading(){
     return(
         <div className='loadingCircle'>
-            <CircularProgress variant="plain" size="lg" color="neutral" />
+            <CircularProgress variant="plain" size="lg" sx={{color:"white"}} />
         </div>
     )
 }

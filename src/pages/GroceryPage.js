865b49f59e50173a994b29211afa37da79897bef
Luke Li <36483872+yhlli@users.noreply.github.com>
2024-06-28 17:39:53 -0400

    api change

--- src/pages/GroceryPage.js
+++ src/pages/GroceryPage.js
@@ -1,8 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../UserContext";
+import { address } from "../Header";
 
 export default function GroceryPage(){
     const {userInfo} = useContext(UserContext);
+    const [grocery, setGrocery] = useState([]);
+    useEffect(()=>{
+        fetch(`${address}/${userInfo.id}/grocerylist`, {
+            method: 'GET',
+            credentials: 'include',
+        }).then(response=>{response.json().then(
+            grocery=>{
+                setGrocery(grocery);
+            }
+        )});
+    },[]);
     return (
         <>
         {userInfo !== null && (

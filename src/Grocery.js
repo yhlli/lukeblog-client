5a4e64782326a91f7a3c749093088a77123d7d66
Luke Li <36483872+yhlli@users.noreply.github.com>
2024-07-09 16:55:03 -0400

    added draggable groceries

--- src/Grocery.js
+++ src/Grocery.js
@@ -1,11 +1,14 @@
 
 
-export default function Grocery({name, quantity, _id, removeGrocery}){
+export default function Grocery({name, quantity, _id, removeGrocery, increment, decrement}){
 
     return(
     <>
         <div className="grocery">
-            <p>{name} (Quantity: {quantity})</p> <button onClick={()=>removeGrocery(_id)}>Delete</button>
+            <p>{name} (Quantity: {quantity})</p>
+            <button onClick={()=>removeGrocery(_id)}>Delete</button>
+            <button onClick={() => increment(name)}>+</button>
+            <button onClick={() => decrement(name,quantity, _id)}>-</button>
         </div>
     </>
     );
--- src/pages/GroceryPage.js
+++ src/pages/GroceryPage.js
@@ -1,13 +1,16 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { UserContext } from "../UserContext";
 import { address } from "../Header";
 import Grocery from "../Grocery";
 
 export default function GroceryPage(){
     const {userInfo} = useContext(UserContext);
-    const [groceryList, setGroceryList] = useState('');
+    const [groceryList, setGroceryList] = useState([]);
     const [groceryItem, setGroceryItem] = useState('');
     const [groceryQuantity, setGroceryQuantity] = useState(1);
+    const dragItem = useRef(0);
+    const draggedItem = useRef(0);
+
     useEffect(()=>{
         fetch(`${address}/${userInfo.id}/grocerylist`, {
             method: 'GET',
@@ -20,7 +23,9 @@ export default function GroceryPage(){
     },[]);
 
     const addGrocery = async (ev)=>{
-        ev.preventDefault();
+        if (ev && typeof ev.preventDefault === 'function') {
+            ev.preventDefault();
+        }
         const data = new FormData();
         data.set('groceryItem', groceryItem);
         data.set('groceryQuantity', groceryQuantity);
@@ -30,7 +35,7 @@ export default function GroceryPage(){
             credentials: 'include',
         });
         if (response.ok){
-            const updatedList = await fetch(`${address}/${userInfo.id}/grocerylist`, {
+            await fetch(`${address}/${userInfo.id}/grocerylist`, {
                 method: 'GET',
                 credentials: 'include',
             }).then(response=>{response.json().then(
@@ -40,6 +45,7 @@ export default function GroceryPage(){
             )});
         }
         setGroceryItem('');
+        setGroceryQuantity(1);
     }
 
     const removeGrocery = async (ev)=>{
@@ -47,8 +53,76 @@ export default function GroceryPage(){
             method: 'DELETE',
             credentials: 'include',
         })
-        if (response.ok){
-            setGroceryList(groceryList.filter(item => item._id !== ev));
+        if (response.ok) {
+            await fetch(`${address}/${userInfo.id}/grocerylist`, {
+                method: 'GET',
+                credentials: 'include',
+            }).then(response => {
+                response.json().then(
+                    grocery => {
+                        setGroceryList(grocery);
+                    }
+                )
+            });
+        }
+    }
+
+    const handleSort = async()=>{
+        if (dragItem.current !== draggedItem.current){
+            const groceryCopy = [...groceryList];
+            const [temp] = groceryCopy.splice(dragItem.current, 1);
+            groceryCopy.splice(draggedItem.current, 0, temp);
+            setGroceryList(groceryCopy);
+            await fetch(`${address}/${userInfo.id}/grocerylist`, {
+                method: 'PUT',
+                body: JSON.stringify(groceryCopy),
+                headers: {'Content-Type': 'application/json'},
+                credentials: 'include',
+            })
+        }
+    }
+
+    const incrementGrocery = async (ev)=>{
+        const num = 1;
+        const response = await fetch(`${address}/${userInfo.id}/grocerylistquantity?num=${num}&name=${ev}`, {
+            method: 'PUT',
+            body: ev,
+            credentials: 'include',
+        })
+        if (response.ok) {
+            await fetch(`${address}/${userInfo.id}/grocerylist`, {
+                method: 'GET',
+                credentials: 'include',
+            }).then(response => {
+                response.json().then(
+                    grocery => {
+                        setGroceryList(grocery);
+                    }
+                )
+            });
+        }
+    }
+
+    const decrementGrocery = async (ev,quantity,id)=>{
+        if (quantity== 1){
+            removeGrocery(id);
+        }
+        const num = -1;
+        const response = await fetch(`${address}/${userInfo.id}/grocerylistquantity?num=${num}&name=${ev}`, {
+            method: 'PUT',
+            credentials: 'include',
+        })
+        if (response.ok) {
+            await fetch(`${address}/${userInfo.id}/grocerylist`, {
+                method: 'GET',
+                credentials: 'include',
+            }).then(response => {
+                response.json().then(
+                    grocery => {
+                        setGroceryList(grocery);
+                    }
+                )
+            });
         }
     }
 
@@ -56,9 +130,18 @@ export default function GroceryPage(){
         <>
         {userInfo !== null && (
             <>
-            {groceryList.length > 0 && groceryList.map(grocery => (
-                <div>
-                    <Grocery {...grocery} key={grocery._id} removeGrocery={removeGrocery} />
+            {groceryList.length > 0 && groceryList.map((grocery,index) => (
+                <div
+                    draggable
+                    onDragStart={()=>(dragItem.current = index)}
+                    onDragEnter={()=>(draggedItem.current = index)}
+                    onDragEnd={handleSort}
+                    onDragOver={(e)=>e.preventDefault()}
+                >
+                    <Grocery {...grocery} key={grocery._id}
+                        removeGrocery={removeGrocery}
+                        increment={incrementGrocery}
+                        decrement={decrementGrocery} />
                 </div>
             ))}
             
